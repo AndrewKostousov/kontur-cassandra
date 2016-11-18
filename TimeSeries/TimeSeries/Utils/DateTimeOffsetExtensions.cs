52f41edfdf251434339aa01f99d04aa5c236b776
poroshin <poroshin>
2016-11-18 12:30:53 +0500

    Min and max timeuuid refactoring

--- TimeSeries/TimeSeries/Utils/DateTimeOffsetExtensions.cs
+++ TimeSeries/TimeSeries/Utils/DateTimeOffsetExtensions.cs
@@ -10,16 +10,20 @@ namespace CassandraTimeSeries
             return new DateTimeOffset(dtoffset.Ticks - dtoffset.Ticks % precise.Ticks, TimeSpan.Zero);
         }
 
+        private static readonly byte[] 
+            MinNode = {0x80, 0x80, 0x80, 0x80, 0x80, 0x80}, 
+            MaxNode = { 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f },
+            MinClockSequence = { 0x00, 0x80 },
+            MaxClockSequence = { 0x3f, 0x7f };
+
         public static TimeUuid MinTimeUuid(this DateTimeOffset date)
         {
-            return TimeUuid.NewId(new byte[] { 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 },
-                new byte[] { 0x00, 0x80 }, date);
+            return TimeUuid.NewId(MinNode, MinClockSequence, date);
         }
 
         public static TimeUuid MaxTimeUuid(this DateTimeOffset date)
         {
-            return TimeUuid.NewId(new byte[] { 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f },
-                new byte[] { 0x3f, 0x7f }, date);
+            return TimeUuid.NewId(MaxNode, MaxClockSequence, date);
         }
     }
 }
