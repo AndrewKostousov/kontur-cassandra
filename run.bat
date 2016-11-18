f471d642e0b7d8bf7bbedcf29cb10f21bc1d356b
poroshin <poroshin>
2016-11-18 10:32:02 +0500

    fix errors due to incorrect value of JAVA_HOME

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_112
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO set CurVer=%%B
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment\%CurVer%" /v JavaHome') DO set JAVA_HOME=%%B
 
 set LOGS_DIR=Cassandra\logs
 set DATA_DIR=Cassandra\db\data
