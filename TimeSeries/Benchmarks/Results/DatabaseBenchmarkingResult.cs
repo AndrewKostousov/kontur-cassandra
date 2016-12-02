5ffa408dcc8e8121613c47af9605d6f6139271e7
Poroshin Ivan <niemandkun@yandex.ru>
2016-12-02 17:18:58 +0500

    DatabaseBenchmarkingResult refactored

--- TimeSeries/Benchmarks/Results/DatabaseBenchmarkingResult.cs
+++ TimeSeries/Benchmarks/Results/DatabaseBenchmarkingResult.cs
@@ -23,44 +23,57 @@ namespace Benchmarks.Results
 
         public string CreateReport()
         {
-            var averageReadLatency = readers
-                .Select(x => x.AverageLatency)
-                .Average();
+            var totalEventsRead = readers.Sum(x => x.TotalEventsRead);
+            var totalEventsWritten = writers.Sum(x => x.TotalEventsWritten);
 
+            var statistics = "\n";
+
+            if (totalEventsRead != 0)
+                statistics += GetReadStatistics();
+            if (totalEventsWritten != 0 && totalEventsRead != 0)
+                statistics += "\n\n";
+            if (totalEventsWritten != 0)
+                statistics += GetWriteStatistics();
+
+            return statistics;
+        }
+
+        private string GetWriteStatistics()
+        {
             var averageWriteLatency = writers
                 .Select(x => x.AverageLatency)
                 .Average();
 
-            var totalReadTime = readers.Select(x => x.TotalTime).Average();
-            var totalWriteTime = writers.Select(x => x.TotalTime).Average();
-
-            var totalReadsCount = readers.Sum(x => x.TotalReadsCount);
-            var totalEventsRead = readers.Sum(x => x.TotalEventsRead);
             var totalEventsWritten = writers.Sum(x => x.TotalEventsWritten);
-
-            var readThroughput = totalEventsRead / totalReadTime.TotalSeconds;
+            var totalWriteTime = writers.Select(x => x.TotalTime).Average();
             var writeThroughput = totalEventsWritten / totalWriteTime.TotalSeconds;
-            
-            var statistics = "\n";
+            var writesByThread = string.Join(", ", writers.Select(x => x.TotalEventsWritten));
 
-            if (totalEventsRead != 0)
-                statistics +=
-                    $"Average read latency: {averageReadLatency}\n" +
-                    $"Total reads count: {totalReadsCount}\n" +
-                    $"Total events read: {totalEventsRead}\n" +
-                    $"Total events read by single thread: {string.Join(", ", readers.Select(x => x.TotalEventsRead))}\n" +
-                    $"Read throughput: {readThroughput} events per second";
-
-            if (totalEventsWritten != 0)
-                statistics += "\n\n" +
-                    $"Average write latency: {averageWriteLatency}\n" +
-                    $"Total events written: {totalEventsWritten}\n" +
-                    $"Total events written by single thread: {string.Join(", ", writers.Select(x => x.TotalEventsWritten))}\n" +
-                    $"Write throughput: {writeThroughput} events per second";
-
-            return statistics;
+            return $"Average write latency: {averageWriteLatency}\n" +
+                   $"Total events written: {totalEventsWritten}\n" +
+                   $"Total events written by single thread: {writesByThread}\n" +
+                   $"Write throughput: {writeThroughput} events per second";
         }
-        
+
+        private string GetReadStatistics()
+        {
+            var averageReadLatency = readers
+                .Select(x => x.AverageLatency)
+                .Average();
+
+            var totalEventsRead = readers.Sum(x => x.TotalEventsRead);
+            var totalReadTime = readers.Select(x => x.TotalTime).Average();
+            var totalReadsCount = readers.Sum(x => x.TotalReadsCount);
+            var readThroughput = totalEventsRead / totalReadTime.TotalSeconds;
+            var readsByThread = string.Join(", ", readers.Select(x => x.TotalEventsRead));
+
+            return $"Average read latency: {averageReadLatency}\n" +
+                   $"Total reads count: {totalReadsCount}\n" +
+                   $"Total events read: {totalEventsRead}\n" +
+                   $"Total events read by single thread: {readsByThread}\n" +
+                   $"Read throughput: {readThroughput} events per second";
+        }
+
         public IBenchmarkingResult Update(IBenchmarkingResult otherResult)
         {
             var other = otherResult as DatabaseBenchmarkingResult;
