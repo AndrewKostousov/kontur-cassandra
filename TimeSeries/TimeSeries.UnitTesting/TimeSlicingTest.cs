c38bcadafb97dc7eb253d699c9c45af3f99dd2c9
Poroshin Ivan <niemandkun@yandex.ru>
2016-12-09 18:49:03 +0500

    example of better approach to unit testing

--- TimeSeries/TimeSeries.UnitTesting/TimeSlicingTest.cs
+++ TimeSeries/TimeSeries.UnitTesting/TimeSlicingTest.cs
@@ -10,6 +10,35 @@ namespace CassandraTimeSeries.UnitTesting
     [TestFixture]
     public class TimeSlicingTest
     {
+        [SetUp]
+        public void SetUp()
+        {
+            t1 = Timestamp.Now;
+            sliceDuration = TimeSpan.FromMinutes(1);
+            firstSliceStart = t1.Floor(sliceDuration);
+            secondSliceStart = firstSliceStart + sliceDuration;
+        }
+
+        [Test]
+        public void UpperBound_IsLess_Than_LowerBound()
+        {
+            var t1 = Timestamp.Now;
+            var t2 = t1 - sliceDuration;
+            TimeSlicer.Slice(t1, t2, sliceDuration).Should().BeEmpty();
+        }
+
+        [Test]
+        public void UpperBound_IsEqual_To_LowerBound()
+        {
+            TimeSlicer.Slice(t1, t1, sliceDuration).Should().Equal(firstSliceStart);
+        }
+
+        [Test]
+        public void Generic()
+        {
+            TimeSlicer.Slice(t1, t1+sliceDuration.Multiply(2), sliceDuration).Should().Equal(firstSliceStart, secondSliceStart, firstSliceStart+ sliceDuration.Multiply(2));
+        }
+
         static readonly object[] testDataSource = 
         {
             new[] { "00:00:00 +00:00", "00:11:00 +00:00" },
@@ -18,6 +47,11 @@ namespace CassandraTimeSeries.UnitTesting
             new[] { "00:00:11 +00:00", "00:00:11 +00:00" },
             new[] { "00:00:11 +00:00", "00:11:11 +00:00" },
         };
+
+        private TimeSpan sliceDuration;
+        private Timestamp firstSliceStart;
+        private Timestamp secondSliceStart;
+        private Timestamp t1;
 
         [TestCaseSource(nameof(testDataSource))]
         public void Slicing_ShouldIncludeStart(string startTime, string endTime)
