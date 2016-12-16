f48626339cb642a90e927679fd106cde4d93332d
Poroshin Ivan <niemandkun@yandex.ru>
2016-12-16 12:21:59 +0500

    start - exclusive, end - inclusive in ITimeSeries

--- TimeSeries/TimeSeries/Interfaces/ITimeSeries.cs
+++ TimeSeries/TimeSeries/Interfaces/ITimeSeries.cs
@@ -8,7 +8,7 @@ namespace CassandraTimeSeries.Interfaces
     public interface ITimeSeries
     {
         void Write(Event ev);
-        List<Event> ReadRange(Timestamp startInclusive, Timestamp endExclusive, int count=1000);
-        List<Event> ReadRange(TimeGuid startInclusive, TimeGuid endExclusive, int count=1000);
+        List<Event> ReadRange(Timestamp startExclusive, Timestamp endInclusive, int count=1000);
+        List<Event> ReadRange(TimeGuid startExclusive, TimeGuid endInclusive, int count=1000);
     }
 }
--- TimeSeries/TimeSeries/Model/TimeSeries.cs
+++ TimeSeries/TimeSeries/Model/TimeSeries.cs
@@ -25,49 +25,35 @@ namespace CassandraTimeSeries.Model
             table.Insert(ev).Execute();
         }
 
-        public List<Event> ReadRange(Timestamp startInclusive, Timestamp endExclusive, int count = 1000)
+        public List<Event> ReadRange(Timestamp startExclusive, Timestamp endInclusive, int count = 1000)
         {
-            var startGuid = startInclusive?.MinTimeGuid();
+            var startGuid = startExclusive?.MaxTimeGuid();
+            var endGuid = endInclusive?.MaxTimeGuid();
 
-            var endGuid = startInclusive == endExclusive 
-                ? endExclusive?.MaxTimeGuid()
-                : endExclusive?.MinTimeGuid();
-            
             return ReadRange(startGuid, endGuid, count);
         }
 
-        public List<Event> ReadRange(TimeGuid startInclusive, TimeGuid endExclusive, int count = 1000)
+        public List<Event> ReadRange(TimeGuid startExclusive, TimeGuid endInclusive, int count = 1000)
         {
-            var start = startInclusive ?? TimeGuid.MinValue;
-            var end = endExclusive ?? TimeGuid.MaxValue;
+            var start = startExclusive?.ToTimeUuid();
+            var end = endInclusive?.ToTimeUuid();
+
+            if (!start.HasValue && !end.HasValue)
+                return table.Take(count).Execute().ToList();
+
+            if (!start.HasValue)
+                return table.Where(e => e.Id.CompareTo(end.Value) <= 0).Take(count).Execute().ToList();
+
+            if (!end.HasValue)
+                return table.Where(e => e.Id.CompareTo(start.Value) > 0).Take(count).Execute().ToList();
             
-            return TimeSlicer.Slice(start.GetTimestamp(), end.GetTimestamp(), Event.SliceDutation)
-                .Select(sliceId => GetIdCondition(startInclusive?.ToTimeUuid(), endExclusive?.ToTimeUuid(), sliceId.Ticks))
-                .SelectMany(condition => GetRangeFromTable(condition, count))
+            return TimeSlicer.Slice(startExclusive.GetTimestamp(), endInclusive.GetTimestamp(), Event.SliceDutation)
+                .SelectMany(sliceId => table
+                    .Where(e => e.SliceId == sliceId.Ticks && e.Id.CompareTo(start.Value) > 0 && e.Id.CompareTo(end.Value) <= 0)
+                    .Take(count)
+                    .Execute())
                 .Take(count)
                 .ToList();
-        }
-
-        private IEnumerable<Event> GetRangeFromTable(Expression<Func<Event, bool>> condition, int count)
-        {
-            return table
-                .Where(condition)
-                .Take(count)
-                .Execute();
-        }
-
-        private Expression<Func<Event, bool>> GetIdCondition(TimeUuid? start, TimeUuid? end, long sliceId)
-        {
-            if (start == null && end == null)
-                return e => e.SliceId == sliceId;
-            if (end == null)
-                return e => e.SliceId == sliceId && e.Id.CompareTo(start.Value) >= 0;
-            if (start == null)
-                return e => e.SliceId == sliceId && e.Id.CompareTo(end.Value) < 0;
-            if (start == end)
-                return e => e.SliceId == sliceId && e.Id.CompareTo(start.Value) == 0;
-
-            return e => e.SliceId == sliceId && e.Id.CompareTo(start.Value) >= 0 && e.Id.CompareTo(end.Value) < 0;
         }
     }
 }
