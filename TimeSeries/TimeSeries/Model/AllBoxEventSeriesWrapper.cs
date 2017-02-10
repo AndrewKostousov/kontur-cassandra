4daf61e016dcf451bbd83fae5b651bb7b162dff5
Poroshin Ivan <niemandkun@yandex.ru>
2017-02-10 15:05:15 +0500

    + reference EdiTimeline

---
+++ TimeSeries/TimeSeries/Model/AllBoxEventSeriesWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CassandraTimeSeries.Interfaces;
+using Commons;
+using Commons.TimeBasedUuid;
+
+namespace CassandraTimeSeries.Model
+{
+    class AllBoxEventSeriesWrapper : ITimeSeries
+    {
+        public Event Write(EventProto ev)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> ReadRange(Timestamp startExclusive, Timestamp endInclusive, int count = 1000)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> ReadRange(TimeGuid startExclusive, TimeGuid endInclusive, int count = 1000)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
